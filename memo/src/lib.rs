7110280517441108ddeb60520d31206a77c2aebc
Sebastian <sfrueh@gmx.net>
2026-01-21 10:59:54 +0100

    feat(memo): Add done / pending state handling & purge for done memos

--- memo/src/lib.rs
+++ memo/src/lib.rs
@@ -1,21 +1,79 @@
 use std::{
+    fmt::Display,
     fs::{self, File},
-    io::{BufRead, BufReader, Result, Write},
-    path::Path,
+    io::{BufReader, BufWriter, Result},
+    path::{Path, PathBuf},
 };
 
-fn open(path: impl AsRef<Path>) -> Result<Vec<String>> {
-    if fs::exists(&path)? {
-        let file = BufReader::new(File::open(&path)?);
-        file.lines().collect()
-    } else {
-        Ok(Vec::new())
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Memos {
+    path: PathBuf,
+    pub inner: Vec<Memo>,
+}
+
+impl Memos {
+    pub fn open(path: impl AsRef<Path>) -> Result<Self> {
+        let mut memos = Self {
+            path: path.as_ref().to_path_buf(),
+            inner: Vec::new(),
+        };
+        if fs::exists(&path)? {
+            let file = BufReader::new(File::open(&path)?);
+            // memos.inner = file.lines().collect::<Result<Vec<_>>>()?;
+            memos.inner = serde_json::from_reader(file)?;
+        }
+        Ok(memos)
+    }
+
+    pub fn sync(&self) -> Result<()> {
+        let file = File::create(&self.path)?;
+        serde_json::to_writer(BufWriter::new(file), &self.inner)?;
+        Ok(())
+    }
+
+    pub fn find_all(&mut self, arg: &str) -> Vec<&mut Memo> {
+        self.inner
+            .iter_mut()
+            .filter(|m| m.status == Status::Pending && m.text.contains(arg))
+            .collect()
+    }
+
+    pub fn purge_done(&mut self) {
+        self.inner.retain(|m| m.status != Status::Done);
     }
 }
 
-fn sync(memo: &Vec<String>, path: impl AsRef<Path>) -> Result<()> {
-    let mut file = File::options().create(true).append(true).open(path)?;
-    writeln!(file, "{}", memo.join("\n"))
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Memo {
+    pub text: String,
+    pub status: Status,
+}
+
+impl Display for Memo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.status, self.text)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum Status {
+    Pending,
+    Done,
+}
+
+impl Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Pending => "-",
+                Self::Done => "x",
+            }
+        )
+    }
 }
 
 #[cfg(test)]
@@ -25,35 +83,82 @@ mod test {
     use super::*;
 
     #[test]
-    fn open_returns_data_from_given_file() {
-        let memos = open("./tests/data/test_file_1").unwrap();
-        assert_eq!(memos, vec!["foo", "bar"], "wrong data");
-    }
-
-    #[test]
-    fn open_returns_empty_vec_for_missing_file() {
-        let memos = open("bogus.txt").unwrap();
-        assert!(memos.is_empty(), "vec not empty");
-    }
-
-    #[test]
-    fn sync_creates_file_if_necessary() {
+    fn round_trip_via_sync_and_open_preserves_data() {
         let dir = tempdir().unwrap();
-        let path = dir.path().join("memo.txt");
-        let memos = vec!["foo".to_string(), "bar".to_string()];
-        sync(&memos, &path).unwrap();
-        let text = fs::read_to_string(path).unwrap();
-        assert_eq!(text, "foo\nbar\n", "wrong text");
+        let path = dir.path().join("memo.json");
+        let memos = Memos {
+            path: path.clone(),
+            inner: vec![
+                Memo {
+                    text: "foo".to_string(),
+                    status: Status::Pending,
+                },
+                Memo {
+                    text: "bar".to_string(),
+                    status: Status::Pending,
+                },
+            ],
+        };
+        memos.sync().unwrap();
+        let memos_2 = Memos::open(&path).unwrap();
+        assert_eq!(memos.inner, memos_2.inner, "wrong text");
     }
 
     #[test]
-    fn sync_appends_to_existing_file() {
-        let dir = tempdir().unwrap();
-        let path = dir.path().join("memo.txt");
-        fs::write(&path, "foo\n").unwrap();
-        let memo = vec!["bar".to_string()];
-        sync(&memo, &path).unwrap();
-        let text = fs::read_to_string(path).unwrap();
-        assert_eq!(text, "foo\nbar\n", "wrong text");
+    fn find_all_return_all_memo_with_matching_text() {
+        let mut memos = Memos {
+            path: PathBuf::new(),
+            inner: vec![
+                Memo {
+                    text: "foo".to_string(),
+                    status: Status::Pending,
+                },
+                Memo {
+                    text: "_foo".to_string(),
+                    status: Status::Pending,
+                },
+                Memo {
+                    text: "bar".to_string(),
+                    status: Status::Pending,
+                },
+                Memo {
+                    text: "foo".to_string(),
+                    status: Status::Done,
+                },
+            ],
+        };
+        let found: Vec<&mut Memo> = memos.find_all("foo");
+        assert_eq!(found.len(), 2, "wrong number of matches");
+        assert_eq!(found[0].text, "foo");
+        assert_eq!(found[1].text, "_foo");
+    }
+
+    #[test]
+    fn purge_done_removes_all_done_tasks() {
+        let mut memos = Memos {
+            path: PathBuf::new(),
+            inner: vec![
+                Memo {
+                    text: "foo".to_string(),
+                    status: Status::Pending,
+                },
+                Memo {
+                    text: "_foo".to_string(),
+                    status: Status::Pending,
+                },
+                Memo {
+                    text: "bar".to_string(),
+                    status: Status::Done,
+                },
+                Memo {
+                    text: "foo".to_string(),
+                    status: Status::Done,
+                },
+            ],
+        };
+        memos.purge_done();
+        assert_eq!(memos.inner.len(), 2, "wrong number of matches");
+        assert_eq!(memos.inner[0].text, "foo");
+        assert_eq!(memos.inner[1].text, "_foo");
     }
 }
--- memo/src/main.rs
+++ memo/src/main.rs
@@ -1,20 +1,44 @@
 use anyhow::Result;
+use clap::Parser;
 
-use std::env;
+use memo::{Memo, Memos, Status};
 
-use memo::{open, sync};
-
+#[derive(Parser)]
+/// Stores and manages simple reminders.
+struct Args {
+    #[arg(short, long)]
+    /// Marks all matching memos as done
+    done: bool,
+    #[arg(short, long)]
+    /// Purges all done memos
+    purge: bool,
+    /// Text of the memo to store or mark as done
+    text: Vec<String>,
+}
 fn main() -> Result<()> {
-    let mut memos = open("memos.txt")?;
-    let args: Vec<_> = env::args().skip(1).collect();
-    if args.is_empty() {
-        for memo in &memos {
+    let args = Args::parse();
+    let mut memos = Memos::open("memos.json")?;
+    let text = args.text.join(" ");
+    if args.purge {
+        memos.purge_done();
+        memos.sync()?;
+    }
+    if args.done {
+        for m in memos.find_all(&text) {
+            m.status = Status::Done;
+            println!("Marked '{}' as done.", m.text);
+        }
+        memos.sync()?;
+    } else if args.text.is_empty() {
+        for memo in &memos.inner {
             println!("{memo}");
         }
     } else {
-        let memo = args.join(" ");
-        memos.push(memo);
-        sync(&memos, "memos.txt");
+        memos.inner.push(Memo {
+            text,
+            status: Status::Pending,
+        });
+        memos.sync()?;
     }
     Ok(())
 }
